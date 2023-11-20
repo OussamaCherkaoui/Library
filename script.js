5573669f8cee483e949e3e650f8111b52f826c5f
OussamaCherkaoui <oussamacherkaoui2019@gmail.com>
2023-11-20 14:02:10 +0000

    use class instead of plain constructors

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
-const myLibrary={
-    books: [],
-    addBook: function (book) {
-        this.books.push(book);
-      },
-};
+class Library {
+    constructor() {
+      this.books = [];
+    }
 
-Book.prototype.toggleReadStatus = function () {
-    this.isread = !this.isread;
-};
-
-
-function Book(title,author,numberpages,read){
-    this.title=title;
-    this.author=author;
-    this.numberpages=numberpages;
+    addBook(book) {
+      this.books.push(book);
+    }
 }
 
+class Book {
+    constructor(title, author, numberPages) {
+      this.title = title;
+      this.author = author;
+      this.numberPages = numberPages;
+      this.isRead = false;
+    }
+
+    toggleReadStatus() {
+      this.isRead = !this.isRead;
+    }
+}
+
+
+const myLibrary = new Library();
 const bookForm = document.getElementById('bookForm');
-
-function addBookToLibrary(){
-    myLibrary.addBook(newBook);
-}
-    const table=document.getElementById('table-display');
+const table=document.getElementById('table-display');
 
 
 bookForm.addEventListener('submit', function (e){
+    
     e.preventDefault();
-    const title = document.getElementById('title');
-    const author = document.getElementById('author');
-    const nombrepages = document.getElementById('nbpage');
 
-    const newBook = new Book(title.value, author.value, nombrepages.value);
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const nombrepages = document.getElementById('nbpage').value;
+
+    const newBook = new Book(title,author,nombrepages);
     myLibrary.addBook(newBook);
     
     const row=document.createElement('tr');
-    const rowData = [newBook.title, newBook.author, newBook.numberpages];
+    const rowData = [newBook.title, newBook.author, newBook.numberPages];
+
     rowData.forEach(data => {
         const cell = document.createElement('td');
         cell.textContent = data;
@@ -54,12 +60,21 @@ bookForm.addEventListener('submit', function (e){
     const toggleButton = document.createElement('button');
     toggleButton.textContent = 'Mark as unread';
     toggleButton.classList.add('toggle-button');
-    toggleButton.style.cssText='cursor: pointer;color:white ;background-color: red;border-radius: 6px;border-style: none;padding:3px';
+    toggleButton.style.cssText='cursor: pointer;color:white ;background-color: red;border-radius: 6px;border-style: none;padding:3px;';
     toggleButton.addEventListener('click', function () {
-        newBook.toggleReadStatus(); // Basculez l'état de lecture du livre
-        // Mettez à jour le texte du bouton en fonction du nouvel état
-        toggleButton.style.cssText='cursor: pointer;color:white ;background-color: green;border-radius: 6px;border-style: none;padding:3px';
-        toggleButton.textContent = newBook.isRead ? 'Mark as unread' : 'Mark as read';
+        if(newBook.isRead===false)
+        {
+            newBook.toggleReadStatus(); // Basculez l'état de lecture du livre
+            // Mettez à jour le texte du bouton en fonction du nouvel état
+            toggleButton.style.cssText='cursor: pointer; color:white; background-color:green; border-radius:6px; border-style:none; padding:3px;';
+            toggleButton.textContent = 'Mark as read';
+        }
+        else if(newBook.isRead===true)
+        {
+            newBook.toggleReadStatus();
+            toggleButton.style.cssText='cursor: pointer;color:white ;background-color: red;border-radius: 6px;border-style: none;padding:3px;';
+            toggleButton.textContent = 'Mark as unread';
+        }
     });
 
     
